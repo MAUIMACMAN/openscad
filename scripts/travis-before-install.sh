d734d57b0e3d7b04f862f51707c755b7ca9dd402
Torsten Paul <Torsten.Paul@gmx.de>
2016-10-10 05:28:21 +0200

    Restore build matrix and run script as root.

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-set -x
-
-id
-
 DIST="$1"
 
 #
-# Simple way for testing, e.g. replace command calls with echo
-# SUDO=echo WGET=echo ./scripts/travis-before-install.sh trusty
+# This script must be run as root to install packages.
 #
-SUDO=${SUDO:-sudo}
+# Simple way for testing, e.g. replace command calls
+# with echo.
+#
+# WGET=echo ./scripts/travis-before-install.sh trusty
+#
 WGET=${WGET:-wget}
 
 QUIET="-qq"
 
-echo "DIST = $DIST / Running on:"
+echo "DIST = $DIST / id = $(id) / Running on:"
 echo
 cat /etc/os-release
 echo
 
-"$WGET" -qO - http://files.openscad.org/OBS-Repository-Key.pub | "$SUDO" apt-key add -
+"$WGET" -qO - http://files.openscad.org/OBS-Repository-Key.pub | apt-key add -
 
 PACKAGES='
 	build-essential
@@ -49,33 +48,33 @@ PACKAGES='
 # Purge fglrx driver if that is installed by default
 # as this contains a libGL that breaks the GLX extension
 # in Xvfb causing all graphical tests to fail.
-"$SUDO" apt-get update "$QUIET"
-"$SUDO" apt-get purge "$QUIET" fglrx
+apt-get update "$QUIET"
+apt-get purge "$QUIET" fglrx
 
 if [[ "$DIST" == "trusty" ]]
 then
 
 	echo "Adding external repositories for $DIST"
-	echo 'yes' | "$SUDO" add-apt-repository 'deb http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_14.04/ ./'
+	echo 'yes' | add-apt-repository 'deb http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_14.04/ ./'
 
-	"$SUDO" apt-get update "$QUIET"
-	"$SUDO" apt-get install "$QUIET" $PACKAGES libharfbuzz-dev lib3mf-dev
+	apt-get update "$QUIET"
+	apt-get install "$QUIET" $PACKAGES libharfbuzz-dev lib3mf-dev
 
 elif [[ "$DIST" == "precise" ]]
 then
 
-	echo 'yes' | "$SUDO" add-apt-repository ppa:chrysn/openscad
-	echo 'yes' | "$SUDO" add-apt-repository 'deb http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_12.04/ ./'
-	"$SUDO" apt-get update "$QUIET"
-	"$SUDO" apt-get install "$QUIET" $PACKAGES lib3mf-dev
+	echo 'yes' | add-apt-repository ppa:chrysn/openscad
+	echo 'yes' | add-apt-repository 'deb http://download.opensuse.org/repositories/home:/t-paul:/lib3mf/xUbuntu_12.04/ ./'
+	apt-get update "$QUIET"
+	apt-get install "$QUIET" $PACKAGES lib3mf-dev
 
-	echo 'yes' | "$SUDO" add-apt-repository ppa:mapnik/nightly-trunk
-	"$SUDO" apt-get update "$QUIET"
-	"$SUDO" apt-get install "$QUIET" libharfbuzz-dev
+	echo 'yes' | add-apt-repository ppa:mapnik/nightly-trunk
+	apt-get update "$QUIET"
+	apt-get install "$QUIET" libharfbuzz-dev
 
-	echo 'yes' | "$SUDO" add-apt-repository ppa:oibaf/graphics-drivers
-	"$SUDO" apt-get update "$QUIET"
-	"$SUDO" apt-get install "$QUIET" --install-recommends libgl1-mesa-dev-lts-quantal
+	echo 'yes' | add-apt-repository ppa:oibaf/graphics-drivers
+	apt-get update "$QUIET"
+	apt-get install "$QUIET" --install-recommends libgl1-mesa-dev-lts-quantal
 
 else
 
